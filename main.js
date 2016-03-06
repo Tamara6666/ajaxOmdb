8a3af3ac680bba14cb720b7eb064d1be41866c05
Tamara6666 <tamarafrankel@tamaras-MacBook-Pro.local>
2016-03-06 01:56:43 -0800

    edit

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 $(document).ready(init);
 
+var dataArray = [];
 
 
 function init(){
 	console.log("loaded");
 	$('#click').click(clickButton);
 	$('#click').click(urlHandler);
-	
+//	$('#click').click(dataObj);
 
-	
+
+
 }
 
 
-function clickButton(){ 
+function clickButton(){
 	var $title = urlHandler();
 
 	$.ajax('http://www.omdbapi.com/?t=' + $title, {
@@ -20,7 +22,7 @@ function clickButton(){
 			console.log(data);
 			makeCard(data);
 
-			
+
 		},
 		error: function(error){
 			console.log(error)
@@ -36,36 +38,50 @@ function makeCard(data){
 	var $title = $('<p>').text(data.Title);
 	var $year = $('<p>').text(data.Year);
 	var $plot = $('<p>').text(data.Plot);
-	$('#secondColumn').append($title);
-	$('#secondColumn').append($year);
-	$('#secondColumn').append($plot);
+//	var $div = $('<div>');
+//	$div.append($img).append($title).append($year).append($plot);
+	//$('#firstColumn').append($img);
+	$('#firstColumn').append($title);
+	$('#firstColumn').append($year);
+	$('#firstColumn').append($plot);
 
 }
 
 function urlHandler() {
-	
+
   var $titleSearch = $('#searchMovie').val().toLowerCase().trim();
-  
+
   if (!($titleSearch)) {
   	var $errorMessage = $('<p>').text("Sorry, That Movie isn't in our Database");
     $('#secondColumn').append($errorMessage);
   }
   return $titleSearch;
-  
+
  console.log("through urlHandler");
 }
 
 
 
-
- function dataObj () {
+ function clickSearch () {
    var $dataObj = data.Search;
+   var $title = urlHandler();
+   searchMovies(title);
+}
+/*
+function searchMovies(title, page) {
+	page = page || 1;
+	//if title is in dataObj, index is not -1, then go through the chain of functions, make api call, etc.; if not, then throw the error msg.
+	if (title !== undefined)
+
+}
+
    if ($dataObj===undefined) {
    	error(error);
    	console.log("That Movie isn't in our Database");
-   } else{
+   } else {
    	urlHandler();
    	makeCard();
    	clickButton();
    }
  }
+*/
